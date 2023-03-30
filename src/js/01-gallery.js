ddc6e85405b6b417ee2c4ba60e1d0174a09546ce
igorekdoroshenko <igorek.doroshenko@gmail.com>
2023-03-30 23:01:57 +0300

    Finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesEl = document.querySelector(".gallery");
+let instance;
+
+const galleryEl = galleryItems.map((image) => `<li class="gallery__item">
+   <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+   </a>
+</li>`).join("");
+
+imagesEl.insertAdjacentHTML("beforeend", galleryEl);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  
+    const formDataJson = JSON.stringify(formData);
+      try {
+    localStorage.setItem(STORAGE_KEY, formDataJson);
+    } catch (e) {
+    console.error('LocalStorage not supported');
+     }
+};
+
+function restoreFormState() {
+  const formDataJson = localStorage.getItem(STORAGE_KEY);
+
+  if (formDataJson) {
+    const formData = JSON.parse(formDataJson);
+    const formElements = form.elements;
+    
+    formElements.email.value = formData.mail;
+    formElements.message.value = formData.message;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', restoreFormState);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formDataJson = localStorage.getItem(STORAGE_KEY);
+  const formData = JSON.parse(formDataJson);
+
+  console.log('Email:', formData.mail);
+  console.log('Message:', formData.message);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  event.currentTarget.reset();
+}
